0467d0552025bfb452f5209a259604b53f0a92e6
Mukund <mukunddtridhyatech@gmail.com>
2022-06-01 17:43:12 +0530

    new start

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Form
+# AngularNestedArray
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.3.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MaterialModule } from './shared/material.module';
-import { QuestionComponent } from './components/question/question.component';
-import { QuestionPopComponent } from './components/question-pop/question-pop.component';
-import { AnswerComponent } from './components/answer/answer.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    QuestionComponent,
-    QuestionPopComponent,
-    AnswerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    MaterialModule,
-    ReactiveFormsModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
