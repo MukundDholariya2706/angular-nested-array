c0edc892e573c026095d145cbf3309eb2599870a
Mukund <mukunddtridhyatech@gmail.com>
2022-06-01 18:15:10 +0530

    question list component-update

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,42 +1,51 @@
+import { QuestionPopupComponent } from './../question-popup/question-popup.component';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
-  
   questionListForm!: FormGroup;
 
   // get Form Controls
-  get f(){
+  get f() {
     return this.questionListForm.controls;
   }
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.initForm();
   }
 
   // form declear
-  private initForm(){
-    this.questionListForm = this.fb.group({
-      
-    })
+  private initForm() {
+    this.questionListForm = this.fb.group({});
   }
 
- //open Question pop-up
+  //open Question pop-up
+  openDialog() {
+    const dialogRef = this.dialog.open(QuestionPopupComponent, {
+      width: '500px',
+    });
 
-  openDialog(){
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!result) {
+        //result is empty
+      }
 
+      if (result) {
+        console.log('result :>> ', result);
+      }
+    });
   }
 
   // submit button
-  onReviewAns(){
+  onReviewAns() {
     console.log(this.questionListForm.value);
   }
-
 }
